ec11c8e3930edae99113eaaff09a630b1a22949e
Johan Svensson <johan.svensson93@hotmail.se>
2017-10-22 11:02:24 +0200

    Added code to the btn move to admin and vice versa.

--- Laboration5/MainWindow.xaml.cs
+++ Laboration5/MainWindow.xaml.cs
@@ -21,11 +21,11 @@ namespace Laboration5
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string emailPattern = @"(\w|\D)+[@](\w|\D)+\.(\w|\D)+";
+        private string emailPattern = @"(\w|\D)+[@](\w|\D)+\.(\w|\D)+";
         public MainWindow()
         {
             InitializeComponent();
-        }   
+        }
         //Add button
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
@@ -46,7 +46,7 @@ namespace Laboration5
         //Change button
         private void btnChange_Click(object sender, RoutedEventArgs e)
         {
-            if(userListBox.SelectedIndex != 0)
+            if (userListBox.SelectedIndex != 0)
             {
                 btnChange.IsEnabled = true;
             }
@@ -74,17 +74,19 @@ namespace Laboration5
         private void userListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             btnRemove.IsEnabled = userListBox.SelectedIndex >= 0;
+            btnMoveUserToAdmin.IsEnabled = userListBox.SelectedIndex >= 0;
+
+            //Shows userinfo from user listbox in label
             if ((User)userListBox.SelectedItem != null)
-                userInfoLabel.Content = "Name: " + ((User)userListBox.SelectedItem).Name + "\n" + "Email: " +
-                                        ((User)userListBox.SelectedItem).Email;
+                userInfoLabel.Content = $"Name: {((User)userListBox.SelectedItem).Name} \nEmail: {((User)userListBox.SelectedItem).Email}";
             else
                 userInfoLabel.Content = string.Empty;
 
+           
         }
         //name text box
         private void nameTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
             if (string.IsNullOrWhiteSpace(nameTextBox.Text) || userListBox.Items.Contains(nameTextBox.Text))
             {
                 btnAdd.IsEnabled = userListBox.SelectedIndex >= 0;
@@ -95,20 +97,17 @@ namespace Laboration5
                     labelNameErrorText.Content = "You can not type the same thing!";
                 if (string.IsNullOrWhiteSpace(nameTextBox.Text))
                     //MessageBox.Show("Must type a name");
-                    labelNameErrorText.Content = "Must type a name";
+                    labelNameErrorText.Content = "Type a name";
             }
             else if (btnAdd != null)
             {
                 btnAdd.IsEnabled = true;
                 labelNameErrorText.Content = "";
             }
-
         }
 
         private void emailTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-          
-
             if (string.IsNullOrWhiteSpace(emailTextBox.Text) || userListBox.Items.Contains(emailTextBox.Text))
             {
 
@@ -118,16 +117,44 @@ namespace Laboration5
                     //MessageBox.Show("Can not contain same text as before");
                     labelEmailErrorText.Content = "You can not type the same thing!";
                 if (string.IsNullOrWhiteSpace(emailTextBox.Text))
-                   // MessageBox.Show("Must type an email adress!");
-                labelEmailErrorText.Content = "Must type email adress!";
+                    // MessageBox.Show("Must type an email adress!");
+                    labelEmailErrorText.Content = "Type an email adress!";
             }
             else if (btnAdd != null)
             {
                 btnAdd.IsEnabled = true;
                 labelEmailErrorText.Content = "";
             }
+        }
 
+        // Knapp för att flytta object från userlist till adminlist
+        // Skall gå att göra när man markerar ett object.
+        // objectet flyttas när man trycker på knappen.
+        private void btnMoveUserToAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            if (userListBox.SelectedIndex != -1)
+            {
+                adminListBox.Items.Add(userListBox.SelectedValue);
+                userListBox.Items.Remove(userListBox.SelectedValue);
+            }
+        }
 
+        private void btnMoveAdminToUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (adminListBox.SelectedIndex != -1)
+            {
+                userListBox.Items.Add(adminListBox.SelectedValue);
+                adminListBox.Items.Remove(adminListBox.SelectedValue);
+            }
+        }
+
+        private void adminListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Shows admin info from admin listbox
+            if ((User)adminListBox.SelectedItem != null)
+                userInfoLabel.Content = $"Name: {((User)adminListBox.SelectedItem).Name} \nEmail: {((User)adminListBox.SelectedItem).Email}";
+            else
+                userInfoLabel.Content = string.Empty;
         }
     }
 }
